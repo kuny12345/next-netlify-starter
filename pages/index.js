496bd72f2fbd906b1a3f2d28755bb81b5f230fb7
kuny12345 <kuny12345@gmail.com>
2024-06-30 22:49:32 +0900

    a

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,61 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <a href="https://blenovi.com/"></a>
+          <a href="https://brovena.com/"></a>
+          <a href="https://glonfer.com/"></a>
+          <a href="https://gremiko.com/"></a>
+          <a href="https://gremino.com/"></a>
+          <a href="https://klenovi.com/"></a>
+          <a href="https://nelfimo.com/"></a>
+          <a href="https://plifero.com/"></a>
+          <a href="https://srofine.com/"></a>
+          <a href="https://zrevino.com/"></a>
+          <a href="https://chimedicineworks.com/"></a>
+          <a href="https://sub1.chimedicineworks.com/"></a>
+          <a href="https://sub2.chimedicineworks.com/"></a>
+          <a href="https://sub3.chimedicineworks.com/"></a>
+          <a href="https://sub4.chimedicineworks.com/"></a>
+          <a href="https://sub5.chimedicineworks.com/"></a>
+          <a href="https://sub6.chimedicineworks.com/"></a>
+          <a href="https://sub7.chimedicineworks.com/"></a>
+          <a href="https://sub8.chimedicineworks.com/"></a>
+          <a href="https://sub9.chimedicineworks.com/"></a>
+          <a href="https://thevuemedia.com/"></a>
+          <a href="https://sub1.thevuemedia.com/"></a>
+          <a href="https://sub2.thevuemedia.com/"></a>
+          <a href="https://sub3.thevuemedia.com/"></a>
+          <a href="https://sub4.thevuemedia.com/"></a>
+          <a href="https://sub5.thevuemedia.com/"></a>
+          <a href="https://sub6.thevuemedia.com/"></a>
+          <a href="https://sub7.thevuemedia.com/"></a>
+          <a href="https://sub8.thevuemedia.com/"></a>
+          <a href="https://sub9.thevuemedia.com/"></a>
+          <a href="https://jentavi.com/"></a>
+          <a href="https://flenaro.com/"></a>
+          <a href="https://zorkini.com/"></a>
+          <a href="https://plovena.com/"></a>
+          <a href="https://brafeno.com/"></a>
+          <a href="https://dynamicsq.com/"></a>
+          <a href="https://euwcx.cc/"></a>
+          <a href="https://backlinkr3.imweb.me/"></a>
+          <a href="https://backlinkr2.imweb.me/" rel="nofollow"></a>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
