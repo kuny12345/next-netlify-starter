3ea67983644464b46c232126636ad35d0ee6895e
kuny12345 <kuny12345@gmail.com>
2024-07-01 16:33:41 +0900

    a

--- pages/index.js
+++ pages/index.js
@@ -13,45 +13,68 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          <a href="https://blenovi.com/"></a>
-          <a href="https://brovena.com/"></a>
-          <a href="https://glonfer.com/"></a>
-          <a href="https://gremiko.com/"></a>
-          <a href="https://gremino.com/"></a>
-          <a href="https://klenovi.com/"></a>
-          <a href="https://nelfimo.com/"></a>
-          <a href="https://plifero.com/"></a>
-          <a href="https://srofine.com/"></a>
-          <a href="https://zrevino.com/"></a>
-          <a href="https://chimedicineworks.com/"></a>
-          <a href="https://sub1.chimedicineworks.com/"></a>
-          <a href="https://sub2.chimedicineworks.com/"></a>
-          <a href="https://sub3.chimedicineworks.com/"></a>
-          <a href="https://sub4.chimedicineworks.com/"></a>
-          <a href="https://sub5.chimedicineworks.com/"></a>
-          <a href="https://sub6.chimedicineworks.com/"></a>
-          <a href="https://sub7.chimedicineworks.com/"></a>
-          <a href="https://sub8.chimedicineworks.com/"></a>
-          <a href="https://sub9.chimedicineworks.com/"></a>
-          <a href="https://thevuemedia.com/"></a>
-          <a href="https://sub1.thevuemedia.com/"></a>
-          <a href="https://sub2.thevuemedia.com/"></a>
-          <a href="https://sub3.thevuemedia.com/"></a>
-          <a href="https://sub4.thevuemedia.com/"></a>
-          <a href="https://sub5.thevuemedia.com/"></a>
-          <a href="https://sub6.thevuemedia.com/"></a>
-          <a href="https://sub7.thevuemedia.com/"></a>
-          <a href="https://sub8.thevuemedia.com/"></a>
-          <a href="https://sub9.thevuemedia.com/"></a>
-          <a href="https://jentavi.com/"></a>
-          <a href="https://flenaro.com/"></a>
-          <a href="https://zorkini.com/"></a>
-          <a href="https://plovena.com/"></a>
-          <a href="https://brafeno.com/"></a>
-          <a href="https://dynamicsq.com/"></a>
-          <a href="https://euwcx.cc/"></a>
-          <a href="https://backlinkr3.imweb.me/"></a>
-          <a href="https://backlinkr2.imweb.me/" rel="nofollow"></a>
+          <a href="https://blenovi.com/">....</a>
+          <a href="https://brovena.com/">....</a>
+          <a href="https://glonfer.com/">....</a>
+          <a href="https://gremiko.com/">....</a>
+          <a href="https://gremino.com/">....</a>
+          <a href="https://klenovi.com/">....</a>
+          <a href="https://nelfimo.com/">....</a>
+          <a href="https://plifero.com/">....</a>
+          <a href="https://srofine.com/">....</a>
+          <a href="https://zrevino.com/">....</a>
+          <a href="https://chimedicineworks.com/">....</a>
+          <a href="https://sub1.chimedicineworks.com/">....</a>
+          <a href="https://sub2.chimedicineworks.com/">....</a>
+          <a href="https://sub3.chimedicineworks.com/">....</a>
+          <a href="https://sub4.chimedicineworks.com/">....</a>
+          <a href="https://sub5.chimedicineworks.com/">....</a>
+          <a href="https://sub6.chimedicineworks.com/">....</a>
+          <a href="https://sub7.chimedicineworks.com/">....</a>
+          <a href="https://sub8.chimedicineworks.com/">....</a>
+          <a href="https://sub9.chimedicineworks.com/">....</a>
+          <a href="https://thevuemedia.com/">....</a>
+          <a href="https://sub1.thevuemedia.com/">....</a>
+          <a href="https://sub2.thevuemedia.com/">....</a>
+          <a href="https://sub3.thevuemedia.com/">....</a>
+          <a href="https://sub4.thevuemedia.com/">....</a>
+          <a href="https://sub5.thevuemedia.com/">....</a>
+          <a href="https://sub6.thevuemedia.com/">....</a>
+          <a href="https://sub7.thevuemedia.com/">....</a>
+          <a href="https://sub8.thevuemedia.com/">....</a>
+          <a href="https://sub9.thevuemedia.com/">....</a>
+          <a href="https://jentavi.com/">....</a>
+          <a href="https://flenaro.com/">....</a>
+          <a href="https://zorkini.com/">....</a>
+          <a href="https://plovena.com/">....</a>
+          <a href="https://brafeno.com/">....</a>
+          <a href="https://dynamicsq.com/">....</a>
+          <a href="https://euwcx.cc/">....</a>
+          <a href="https://backlinkr3.imweb.me/">....</a>
+          <a href="https://q001.brafeno.com">....</a>
+          <a href="https://q002.brafeno.com">....</a>
+          <a href="https://q003.brafeno.com">....</a>
+          <a href="https://q004.brafeno.com">....</a>
+          <a href="https://q005.brafeno.com">....</a>
+          <a href="https://q006.brafeno.com">....</a>
+          <a href="https://q007.brafeno.com">....</a>
+          <a href="https://q008.brafeno.com">....</a>
+          <a href="https://q009.brafeno.com">....</a>
+          <a href="https://q010.brafeno.com">....</a>
+          <a href="https://q011.brafeno.com">....</a>
+          <a href="https://q012.brafeno.com">....</a>
+          <a href="https://q013.brafeno.com">....</a>
+          <a href="https://q014.brafeno.com">....</a>
+          <a href="https://q015.brafeno.com">....</a> 
+          <a href="https://q016.brafeno.com">....</a>
+          <a href="https://q017.brafeno.com">....</a>
+          <a href="https://q018.brafeno.com">....</a>
+          <a href="https://q019.brafeno.com">....</a>
+          <a href="https://q020.brafeno.com">....</a>
+          
+
+
+          <a href="https://backlinkr2.imweb.me/" rel="nofollow">....</a>
         </p>
       </main>
 
